7c821bdbe95f607c42f8e4b0f3df342e3f702c56
dilyanValkov <dilqnvalkov@gmail.com>
2024-06-06 13:01:48 +0300

    Initial commit

---
+++ src/main/java/Valkov/Fishing_Farm_Zasmyano/FishingFarmZasmyanoApplication.java
@@ -0,0 +1,13 @@
+package Valkov.Fishing_Farm_Zasmyano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingFarmZasmyanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingFarmZasmyanoApplication.class, args);
+	}
+
+}
